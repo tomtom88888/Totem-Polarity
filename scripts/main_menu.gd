2ded6f252e4de524b7126d8663a979f1e5e1a293
iii123iii <euyhegnrh@gmail.com>
2024-12-29 18:10:48 +0200

    added some things

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,14 +2,19 @@ class_name MainMenu
 extends Control
 
 @onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton as Button
+@onready var settings_button: Button = $SettingsButton as Button
 @onready var start_level = preload("res://scenes/level_1.tscn") as PackedScene
+@onready var settings = preload("res://scenes/settings.tscn") as PackedScene
 
 func _ready():
 	start_button.button_down.connect(on_start_button_pressed)
+	settings_button.button_down.connect(on_settings_button_pressed)
 
 func on_start_button_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
 
+func on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_packed(settings)
 
 func _on_main_menu_music_finished() -> void:
 	$menu_music.play()
